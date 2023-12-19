0a551919657bd3499ada6a74db2e64efd72045c4
Begimai Mukaeva <begimaimukaeva2001@gmail.com>
2023-12-19 16:58:45 +0600

    after connect api

--- src/components/orders/OrderHere/OrdersHere.jsx
+++ src/components/orders/OrderHere/OrdersHere.jsx
@@ -1,31 +1,159 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from '../OrdersTakeAway/OrdersTakeAway.module.css';
 import img from '../../../img/UserCircle.svg'
 import OrderCardHere from "../../../ui/orderCard2/OrderCardHere";
-
-const initialOrders = [
-    { id: 1, number: 'М-47', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'new' },
-    { id: 2, number: 'М-48', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'inProgress' },
-    { id: 3, number: 'М-49', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'ready' },
-    { id: 4, number: 'М-50', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'canceled' },
-    { id: 5, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 6, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 7, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 8, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 9, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 10, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 11, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 12, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 13, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 14, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-    { id: 15, number: 'М-51', waiterName: 'Мария', items: [{ id: 1, name: 'Капучино', quantity: 1 }, { id: 2, name: 'Багровый закат', quantity: 1 }, { id: 3, name: 'Мохито Клубничный', quantity: 1 }], status: 'completed' },
-];
+import axios from "axios";
 
 const OrdersHere = () => {
-    const [orders, setOrders] = useState(initialOrders);
+    const [orders, setOrders] = useState([]);
     const [currentStatus, setCurrentStatus] = useState('new');
     const [activeStatus, setActiveStatus] = useState('new');
+    const [branchId, setBranchId] = useState(null);
 
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            console.error('Access token is not available.');
+            return;
+        }
+        axios.get('https://muha-backender.org.kg/web/my-branch-id/', {
+            headers: { 'Authorization': `Bearer ${accessToken}` },
+        })
+            .then(response => {
+                setBranchId(response.data.branch_id);
+            })
+            .catch(error => {
+                console.error('Ошибка при получении ID филиала:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            console.error('Access token is not available.');
+            return;
+        }
+
+        if (currentStatus === 'new') {
+            if (!branchId) return;
+            const ws = new WebSocket(`wss://muha-backender.org.kg/ws/new-orders-institution/${branchId}/`);
+            ws.onopen = () => console.log('WebSocket Connection Opened');
+            ws.onerror = error => console.error('WebSocket Error', error);
+            ws.onmessage = e => {
+                const newOrder = JSON.parse(e.data);
+                if (newOrder && newOrder.orders) {
+                    const uniqueNewOrders = newOrder.orders.filter(newOrd =>
+                        !orders.some(ord => ord.id === newOrd.id)
+                    );
+                    if (uniqueNewOrders.length > 0) {
+                        setOrders(orders => [...orders, ...uniqueNewOrders]);
+                    }
+                }
+            };
+
+
+            ws.onclose = () => console.log('WebSocket Connection Closed');
+            return () => {
+                ws.close();
+            };
+        } else if (currentStatus === 'in_progress') {
+            loadInProcessOrders();
+        } else if (currentStatus === 'ready') {
+            loadReadyOrders();
+        } else if (currentStatus === 'canceled') {
+            loadCanceledOrders();
+        } else if (currentStatus === 'completed') {
+            loadCompletedOrders();
+        }
+    }, [currentStatus, branchId, orders]);
+
+    const loadInProcessOrders = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get('https://muha-backender.org.kg/web/institution-orders/in-process/', {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200 && response.data.orders) {
+                setOrders(response.data.orders);
+            } else {
+                console.error('Unexpected response format:', response.data);
+            }
+        } catch (error) {
+            console.error('Error loading in process orders:', error);
+        }
+    };
+
+    const loadReadyOrders = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get('https://muha-backender.org.kg/web/institution-orders/ready/', {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200 && response.data.orders) {
+                setOrders(response.data.orders);
+            } else {
+                console.error('Unexpected response format:', response.data);
+            }
+        } catch (error) {
+            console.error('Error loading in process orders:', error);
+        }
+    };
+
+    const loadCanceledOrders = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get('https://muha-backender.org.kg/web/institution-orders/canceled/', {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200 && response.data.orders) {
+                setOrders(response.data.orders);
+            } else {
+                console.error('Unexpected response format:', response.data);
+            }
+        } catch (error) {
+            console.error('Error loading canceled orders:', error);
+        }
+    };
+
+    const loadCompletedOrders = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get('https://muha-backender.org.kg/web/institution-orders/completed/', {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200 && response.data.orders) {
+                setOrders(response.data.orders);
+            } else {
+                console.error('Unexpected response format:', response.data);
+            }
+        } catch (error) {
+            console.error('Error loading canceled orders:', error);
+        }
+    };
 
     const handleStatusClick = (statusLabel) => {
         const status = statusMap[statusLabel];
@@ -33,19 +161,31 @@ const OrdersHere = () => {
         setActiveStatus(status);
     };
 
+    // const changeOrderStatus = (orderId, newStatus) => {
+    //     const updatedOrders = orders.map(order => {
+    //         if (order.id === orderId) {
+    //             return { ...order, status: newStatus };
+    //         }
+    //         return order;
+    //     });
+    //     setOrders(updatedOrders);
+    // };
+
     const changeOrderStatus = (orderId, newStatus) => {
-        const updatedOrders = orders.map(order => {
-            if (order.id === orderId) {
-                return { ...order, status: newStatus };
+        setOrders(prevOrders => {
+            const updatedOrders = prevOrders.map(order =>
+                order.id === orderId ? { ...order, status: newStatus } : order
+            );
+            if (currentStatus === 'new') {
+                return updatedOrders.filter(order => order.id !== orderId);
             }
-            return order;
+            return updatedOrders;
         });
-        setOrders(updatedOrders);
     };
 
     const statusMap = {
         'Новый': 'new',
-        'В процессе': 'inProgress',
+        'В процессе': 'in_progress',
         'Готово': 'ready',
         'Отменено': 'canceled',
         'Завершено': 'completed',
@@ -61,24 +201,37 @@ const OrdersHere = () => {
 
 
     const renderOrders = () => {
+        console.log('Rendering orders:', orders);
+        if (!Array.isArray(orders)) {
+            console.error('Orders is not an array:', orders);
+            return <div>Загрузка заказов...</div>;
+        }
+
+        const filteredOrders = orders.filter(order => order.status === currentStatus);
+
         return (
             <div className={styles.orderCardsContainer}>
-                {orders
-                    .filter(order => order.status === currentStatus)
-                    .map(order => (
-                        <OrderCardHere
-                            key={order.id}
-                            number={order.number}
-                            waiterName={order.waiterName}
-                            items={order.items}
-                            onAccept={() => changeOrderStatus(order.id, 'inProgress')}
-                            onCancel={() => changeOrderStatus(order.id, 'canceled')}
-                            onReady={() => changeOrderStatus(order.id, 'ready')}
-                            onFinish={() => changeOrderStatus(order.id, 'completed')}
-                            status={order.status}
-                            onOrderCancel={() => handleOrderCancel(order.id)}
-                        />
-                    ))}
+                {/*{orders*/}
+                {/*    .filter(order => order.status === currentStatus)*/}
+                {/*    .map(order => (*/}
+                {filteredOrders.map(order => (
+                    <OrderCardHere
+                        key={order.id || order.number}
+                        number={order.number}
+                        waiterName={order.waiterName}
+                        items={order.items}
+                        // onAccept={() => changeOrderStatus(order.id, 'inProgress')}
+                        // onCancel={() => changeOrderStatus(order.id, 'canceled')}
+                        // onReady={() => changeOrderStatus(order.id, 'ready')}
+                        // onFinish={() => changeOrderStatus(order.id, 'completed')}
+                        handleAcceptClick={() => changeOrderStatus(order.id, 'in_progress')}
+                        onCancel={() => changeOrderStatus(order.id, 'canceled')}
+                        handleReadyOrder={() => changeOrderStatus(order.id, 'ready')}
+                        handleCompleteOrder={() => changeOrderStatus(order.id, 'completed')}
+                        status={order.status}
+                        onOrderCancel={() => handleOrderCancel(order.id)}
+                    />
+                ))}
             </div>
         );
     };
--- src/ui/orderCard2/OrderCardHere.jsx
+++ src/ui/orderCard2/OrderCardHere.jsx
@@ -3,10 +3,95 @@ import styles from '../orderCard1/OrderCard.module.css';
 import OrderCloseModal from '../../components/orders/OrderCloseModal/OrderCloseModal';
 import CardHereInfo from '../../components/orders/CardHereInfo/CardHereInfo';
 import closeOrderCard from '../../img/X.svg';
+import axios from "axios";
 
 const OrderCardHere = ({ number, waiterName, items, onAccept, onCancel, onReady, onFinish, status, onOrderCancel }) => {
     const [isModalOpen, setModalOpen] = useState(false);
     const [isCardInfoModalOpen, setCardInfoModalOpen] = useState(false);
+
+    const handleAcceptClick = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get(`https://muha-backender.org.kg/web/accept-order/?order_id=${number}`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200) {
+                console.log('Order accepted:', response.data);
+                onOrderCancel(number);
+            }
+        } catch (error) {
+            console.error('Error accepting order:', error);
+        }
+    };
+
+    const handleCancelOrder = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get(`https://muha-backender.org.kg/web/cancel-order/?order_id=${number}`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200) {
+                console.log('Order canceled:', response.data);
+                onOrderCancel(number);
+            }
+        } catch (error) {
+            console.error('Error canceling order:', error);
+        }
+    };
+
+    const handleReadyOrder = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get(`https://muha-backender.org.kg/web/make-order-ready/?order_id=${number}`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200) {
+                console.log('Order canceled:', response.data);
+                onOrderCancel(number);
+            }
+        } catch (error) {
+            console.error('Error canceling order:', error);
+        }
+    };
+
+    const handleCompleteOrder = async () => {
+        try {
+            const accessToken = localStorage.getItem('accessToken');
+            if (!accessToken) {
+                console.error('Access token is not available.');
+                return;
+            }
+
+            const response = await axios.get(`https://muha-backender.org.kg/web/complete-order/?order_id=${number}`, {
+                headers: { 'Authorization': `Bearer ${accessToken}` },
+            });
+
+            if (response.status === 200) {
+                console.log('Order canceled:', response.data);
+                onOrderCancel(number);
+            }
+        } catch (error) {
+            console.error('Error canceling order:', error);
+        }
+    };
 
     const handleCancelClick = () => {
         setModalOpen(true);
@@ -34,11 +119,11 @@ const OrderCardHere = ({ number, waiterName, items, onAccept, onCancel, onReady,
     const renderButton = () => {
         switch (status) {
             case 'new':
-                return <button onClick={onAccept} className={styles.orderStateButton1}>Принять</button>;
-            case 'inProgress':
-                return <button onClick={onReady} className={styles.orderStateButton2}>Заказ готов</button>;
+                return <button onClick={handleAcceptClick} className={styles.orderStateButton1}>Принять</button>;
+            case 'in_progress':
+                return <button onClick={handleReadyOrder} className={styles.orderStateButton2}>Заказ готов</button>;
             case 'ready':
-                return <button onClick={onFinish} className={styles.orderStateButton3}>Завершить заказ</button>;
+                return <button onClick={handleCompleteOrder} className={styles.orderStateButton3}>Завершить заказ</button>;
             case 'canceled':
                 return <button className={styles.orderStateButton4}>Заказ отменен</button>;
             case 'completed':
@@ -67,11 +152,13 @@ const OrderCardHere = ({ number, waiterName, items, onAccept, onCancel, onReady,
                 <div onClick={handleCardClick}>
                     <p className={styles.orderWaiterName}>{waiterName}</p>
                     <ul className={styles.orderList}>
-                        {items.map(item => (
-                            <li key={item.id}><span>x{item.quantity}</span>{item.name}</li>
+                        {items.map((item, index) => (
+                            index < 3 && <li key={item.id}><span>x{item.quantity}</span>{item.name}</li>
                         ))}
                     </ul>
-                    <button className={styles.orderListMoreButton}>Еще +2</button>
+                    {items.length > 3 && (
+                        <button className={styles.orderListMoreButton}>Еще +{items.length - 3}</button>
+                    )}
                 </div>
                 <div>
                     {renderButton()}
@@ -79,10 +166,11 @@ const OrderCardHere = ({ number, waiterName, items, onAccept, onCancel, onReady,
             </div>
             {isModalOpen && (
                 <OrderCloseModal
-                    onConfirm={() => {
-                        setModalOpen(false);
-                        onOrderCancel(number);
-                    }}
+                    // onConfirm={() => {
+                    //     setModalOpen(false);
+                    //     onOrderCancel(number);
+                    // }}
+                    onConfirm={handleCancelOrder}
                     onCancel={confirmCloseOrder}
                     orderNumber={number}
                 />
